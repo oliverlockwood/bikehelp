e38c1b377dc83804918c5eb3fee59e8d7e6ab31d
Oliver Lockwood <oliver.lockwood@cantab.net>
2014-10-05 16:01:01 +0100

    Refactor packages and strings

--- src/main/java/com/oliverlockwood/bikehelp/MainActivity.java
+++ src/main/java/com/oliverlockwood/bikehelp/MainActivity.java
@@ -3,7 +3,6 @@ package com.oliverlockwood.bikehelp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import org.hello.R;
 
 public class MainActivity extends Activity {
 
